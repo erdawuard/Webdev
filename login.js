cf0d538fd601325316a82564f342d5af56e48f5c
erdawuard <erdawuard@gmail.com>
2022-05-05 22:59:31 +0200

    dfd

---
+++ login.js
@@ -0,0 +1,8 @@
+$('#startpage, #adstudent').on(
+     'click',
+     function(){
+         $('#startpage, .adstudent').toggle()
+     }
+ )
+
+ https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js
